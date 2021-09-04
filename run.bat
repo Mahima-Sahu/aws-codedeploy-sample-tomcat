87b657c4c2a3aba0ce73ece5ce7b1fac604612d8
Mahima-Sahu <52346587+Mahima-Sahu@users.noreply.github.com>
2021-09-04 14:51:59 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -2,8 +2,10 @@
 
 set myfile="C:\apache-tomcat-8.5.69-windows-x64\apache-tomcat-8.5.69\webapps\SampleMavenTomcatApp.war"
 set myfolder="C:\apache-tomcat-8.5.69-windows-x64\apache-tomcat-8.5.69\webapps\SampleMavenTomcatApp"
-if exist %myfile% del %myfile%
+
 if exist %myfolder% del %myfolder%
+if exist %myfile% del %myfile%
+
 
 start /MIN C:\apache-tomcat-8.5.69-windows-x64\apache-tomcat-8.5.69\bin\startup.bat
 
