d3f61895b7df94415ce33e6b5b40e1cb9da45b04
Mahima-Sahu <52346587+Mahima-Sahu@users.noreply.github.com>
2021-09-04 16:05:56 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -7,6 +7,7 @@ if exist %myfolder% rmdir /S /Q %myfolder%
 if exist %myfile% del %myfile%
 
 start /MIN "" "C:\apache-tomcat-8.5.69-windows-x64\apache-tomcat-8.5.69\bin\startup.bat"
+exit
 echo hii
 
 pause
