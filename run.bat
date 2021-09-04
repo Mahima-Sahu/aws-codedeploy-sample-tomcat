1802d3c0936aba7d71464e433861a474a4032b03
Mahima-Sahu <52346587+Mahima-Sahu@users.noreply.github.com>
2021-09-04 16:10:36 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -6,7 +6,8 @@ set myfolder="C:\apache-tomcat-8.5.69-windows-x64\apache-tomcat-8.5.69\webapps\S
 if exist %myfolder% rmdir /S /Q %myfolder%
 if exist %myfile% del %myfile%
 
-start /MIN "" "C:\apache-tomcat-8.5.69-windows-x64\apache-tomcat-8.5.69\bin\startup.bat"
+::start /MIN "" "C:\apache-tomcat-8.5.69-windows-x64\apache-tomcat-8.5.69\bin\startup.bat"
+call C:\apache-tomcat-8.5.69-windows-x64\apache-tomcat-8.5.69\bin\startup.bat
 exit
 echo hii
 
