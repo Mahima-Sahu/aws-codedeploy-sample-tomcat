790571550d2fdcfc997df374f55fcd3d8cd751bf
Mahima-Sahu <52346587+Mahima-Sahu@users.noreply.github.com>
2021-09-06 17:10:37 +0530

    Update check.bat

--- myscripts/check.bat
+++ myscripts/check.bat
@@ -1,1 +1,27 @@
+@echo off
 
+ECHO %DATE% %TIME% Calling %0 with %*
+set /a count=1
+:start
+SET "URL=http://localhost:8088"
+
+SET HTTP=
+for /f %%a in ( 'curl --write-out "%%{http_code}" -o nul -m 10 -q -s "%URL%"' ) do set HTTP=%%a
+
+
+::echo %HTTP%
+
+if "%HTTP%" == "200" (
+echo Successfully pulled root page with %HTTP% status code
+    exit /b 0
+) else (
+	echo Attempt to curl endpoint returned HTTP Code %HTTP% Backing off and retrying
+	timeout 5
+	set /a count+=1
+	if %count%==3 ( echo failed with %HTTP% error ) else ( goto start )
+	::call C:\Users\mahim\OneDrive\Desktop\url.bat
+    exit /b 1
+)
+
+   
+pause
