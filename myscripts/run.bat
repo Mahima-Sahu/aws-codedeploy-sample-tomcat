26f2130473866e3ef515fb8e24a1e8501a5a23e1
Mahima-Sahu <52346587+Mahima-Sahu@users.noreply.github.com>
2021-09-06 17:51:23 +0530

    Update run.bat

--- myscripts/run.bat
+++ myscripts/run.bat
@@ -12,6 +12,9 @@ if exist %myfolder% rmdir /S /Q %myfolder%
 move %myfile% C:\backup_war
 ::if exist %myfile% del %myfile%
 
+call C:\apache-tomcat-8.5.69-windows-x64\apache-tomcat-8.5.69\bin\startup.bat
+exit
+
 echo hii
 
 pause
