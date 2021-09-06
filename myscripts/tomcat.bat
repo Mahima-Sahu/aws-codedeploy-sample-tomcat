869046fdeaffd32e5019ad267c6d83fa9c71db5e
Mahima-Sahu <52346587+Mahima-Sahu@users.noreply.github.com>
2021-09-06 17:28:04 +0530

    Update tomcat.bat

--- myscripts/tomcat.bat
+++ myscripts/tomcat.bat
@@ -3,6 +3,7 @@
 ::start /MIN "" "C:\apache-tomcat-8.5.69-windows-x64\apache-tomcat-8.5.69\bin\startup.bat"
 call C:\apache-tomcat-8.5.69-windows-x64\apache-tomcat-8.5.69\bin\startup.bat
 exit
+echo hii
 
 pause
 
