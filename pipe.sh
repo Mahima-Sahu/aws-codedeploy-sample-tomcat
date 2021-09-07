9e8db0eafad4485509bc5851c1e6a57dbccaa7eb
Mahima-Sahu <52346587+Mahima-Sahu@users.noreply.github.com>
2021-09-07 14:43:31 +0530

    Update pipe.sh

--- pipe.sh
+++ pipe.sh
@@ -1,28 +1,26 @@
 #!/bin/sh
-sudo apt-get install jq 
-
+#sudo apt-get install jq 
 touch state.json
-
 aws codepipeline get-pipeline-state --name buildpipe > state.json
 STATE=`jq .stageStates[latestExecution.status] state.json`
-
-
-if [ "$STATE" = "InProgress" ]; then
-  echo "InProgress......"
-  timeout 50 
-  aws codepipeline get-pipeline-state --name buildpipe > state.json
-  STATE=`jq .stageStates[latestExecution.status] state.json`  
+if [ "$STATE" = "InProgress" ]
+  then
+    echo "InProgress......"
+    timeout 50 
+    aws codepipeline get-pipeline-state --name buildpipe > state.json
+    STATE=`jq .stageStates[latestExecution.status] state.json`  
 fi
-if [ "$STATE" = "InProgress" ]; then
-  echo "InProgress......"
-  timeout 50 
-  aws codepipeline get-pipeline-state --name buildpipe > state.json
-  STATE=`jq .stageStates[latestExecution.status] state.json`  
+if [ "$STATE" = "InProgress" ]
+  then
+    echo "InProgress......"
+    timeout 50 
+    aws codepipeline get-pipeline-state --name buildpipe > state.json
+    STATE=`jq .stageStates[latestExecution.status] state.json`  
 fi
-
-if [ "$STATE" = "Succeeded" ]; then
-  echo "Pipeline is Succeeded"
-fi
-if [ "$STATE" = "Failed" ]; then
-  echo "Pipeline execution failed !!"
+if [ "$STATE" = "Succeeded" ]
+  then
+    echo "Pipeline is Succeeded"
+elif [ "$STATE" = "Failed" ]
+  then
+    echo "Pipeline execution failed !!"
 fi
