3de52f44dfd94286880e5f4dd58876e43d2fbbed
Mahima-Sahu <52346587+Mahima-Sahu@users.noreply.github.com>
2021-09-07 14:50:51 +0530

    Update pipe.sh

--- pipe.sh
+++ pipe.sh
@@ -1,27 +1,28 @@
 #!/bin/sh
 #sudo apt-get install jq 
-touch state.json
+cat>state.json
+sudo chmod 777 state.json
 aws codepipeline get-pipeline-state --name buildpipe > state.json
 STATE=`jq .latestExecution.status state.json`
 if [ "$STATE" = "InProgress" ]
-  then
-    echo "InProgress......"
-    timeout 50 
-    aws codepipeline get-pipeline-state --name buildpipe > state.json
-    STATE=`jq .latestExecution.status state.json`  
+then
+  echo "InProgress......"
+  timeout 50 
+  aws codepipeline get-pipeline-state --name buildpipe > state.json
+  STATE=`jq .latestExecution.status state.json`  
 fi
 if [ "$STATE" = "InProgress" ]
-  then
-    echo "InProgress......"
-    timeout 50 
-    aws codepipeline get-pipeline-state --name buildpipe > state.json
-    STATE=`jq .latestExecution.status state.json`  
+then
+  echo "InProgress......"
+  timeout 50 
+  aws codepipeline get-pipeline-state --name buildpipe > state.json
+  STATE=`jq .latestExecution.status state.json`  
 fi
 if [ "$STATE" = "Succeeded" ]
-  then
-    echo "Pipeline is Succeeded"
+then
+  echo "Pipeline is Succeeded"
 fi    
 if [ "$STATE" = "Failed" ]
-  then
-    echo "Pipeline execution failed !!"
+then
+  echo "Pipeline execution failed !!"
 fi
