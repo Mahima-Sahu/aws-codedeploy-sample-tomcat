e52c4ac4148c3bb9016b3065f4086005d9e27b54
Mahima-Sahu <52346587+Mahima-Sahu@users.noreply.github.com>
2021-09-08 21:37:16 +0530

    Update unix_pipe.sh

--- unix_pipe.sh
+++ unix_pipe.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 aws codepipeline start-pipeline-execution --name buildpipe
 touch state.json
 aws codepipeline get-pipeline-state --name buildpipe > state.json
 SOURCE=`jq .stageStates[0].latestExecution.status state.json`
-DEPLOY==`jq .stageStates[2].latestExecution.status state.json`
-while [ !([ $SOURCE = "Succeeded" ] && [ $DEPLOY = "Succeeded" ]) ]
+DEPLOY=`jq .stageStates[2].latestExecution.status state.json`
+while [ $SOURCE = "InProgress" ] || [ $DEPLOY = "InProgress" ]
 do
     echo "Pipeline is InProgress......"
     timeout 50 
@@ -12,9 +12,12 @@ do
     SOURCE=`jq .stageStates[0].latestExecution.status state.json`
     DEPLOY=`jq .stageStates[2].latestExecution.status state.json`
 done
-if ([ $SOURCE = "Succeeded" ] && [ $DEPLOY = "Succeeded" ])
+if [ $SOURCE = "Succeeded" ] && [ $DEPLOY = "Succeeded" ]
 then
-    echo "Pipeline Succeeded"
+    echo "Pipeline-- Succeeded --"
+elif [ $SOURCE = "Stopped" ] || [ $DEPLOY = "Stopped" ]
+then
+    echo "Pipeline is-- Stopped --"
 else
-    echo "Pipeline failed"  
+    echo "Pipeline-- Failed --!!"  
 fi
