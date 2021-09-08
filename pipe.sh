dc2625189863e0e5328e9658bff399ea3594c51d
Mahima-Sahu <52346587+Mahima-Sahu@users.noreply.github.com>
2021-09-08 14:13:55 +0530

    Update pipe.sh

--- pipe.sh
+++ pipe.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #sudo apt-get install jq 
-sudo apt-get update -y
-sudo apt-get install -y dos2unix
-touch state.json
+sudo apt-get upgrade
+sudo apt-get install dos2unix
+touch ${{ github.workspace }}/state.json
 aws codepipeline get-pipeline-state --name buildpipe > state.json
-sudo chmod a+x state.json
+#sudo chmod a+x state.json
 STATE=`jq .stageStates[0].latestExecution.status state.json`
 if [ "$STATE" == "InProgress" ]
 then
