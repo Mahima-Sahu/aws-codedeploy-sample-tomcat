8fc95b1d36138ed0af1226fceb4274db00d524cf
Mahima-Sahu <52346587+Mahima-Sahu@users.noreply.github.com>
2021-09-08 13:17:51 +0530

    Update pipe.sh

--- pipe.sh
+++ pipe.sh
@@ -2,14 +2,15 @@
 #sudo apt-get install jq 
 sudo apt-get update -y
 sudo apt-get install -y dos2unix
+touch state.json
 aws codepipeline get-pipeline-state --name buildpipe > state.json
 sudo chmod 777 state.json
-STATE=`jq .latestExecution.status state.json`
+STATE=`jq .stageStates[0].latestExecution.status state.json`
 if [ "$STATE" == "InProgress" ]
 then
   echo "InProgress......"
   timeout 50 
   aws codepipeline get-pipeline-state --name buildpipe > state.json
   sudo chmod 777 state.json
-  STATE=`jq .latestExecution.status state.json`  
+  STATE=`jq .stageStates[0].latestExecution.status state.json`  
 fi
