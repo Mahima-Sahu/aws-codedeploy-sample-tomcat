3d9aa65bac43a076375781670faf265e30ce88bd
Mahima-Sahu <52346587+Mahima-Sahu@users.noreply.github.com>
2021-09-08 17:41:56 +0530

    Add files via upload

---
+++ unix_pipe.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+touch state.json
+aws codepipeline get-pipeline-state --name buildpipe > state.json
+sudo chmod 777 state.json
+STATE=`jq .stageStates[0].latestExecution.status state.json`
+if [ $STATE = "InProgress" ]
+  then echo "InProgress......"
+    timeout 50 
+    aws codepipeline get-pipeline-state --name buildpipe > state.json
+    sudo chmod 777 state.json
+    STATE=`jq .stageStates[0].latestExecution.status state.json`
+  else
+    echo "successfull....."  
+fi
