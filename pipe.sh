fd795d124465e7298aab18f070f5ac287828ba39
Mahima-Sahu <52346587+Mahima-Sahu@users.noreply.github.com>
2021-09-08 17:30:05 +0530

    Update pipe.sh

--- pipe.sh
+++ pipe.sh
@@ -4,8 +4,7 @@ aws codepipeline get-pipeline-state --name buildpipe > state.json
 sudo chmod 777 state.json
 STATE=`jq .stageStates[0].latestExecution.status state.json`
 if [ $STATE = "InProgress" ]
- then
-   echo "InProgress......"
+ then echo "InProgress......"
    timeout 50 
    aws codepipeline get-pipeline-state --name buildpipe > state.json
    sudo chmod 777 state.json
