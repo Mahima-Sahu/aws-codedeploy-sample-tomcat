78079bf3ed134960fc7b50e8111a566611cd518a
Mahima-Sahu <52346587+Mahima-Sahu@users.noreply.github.com>
2021-09-08 12:30:44 +0530

    Update pipe.sh

--- pipe.sh
+++ pipe.sh
@@ -2,24 +2,11 @@
 #sudo apt-get install jq 
 aws codepipeline get-pipeline-state --name buildpipe > state.json
 sudo chmod 777 state.json
-STATE=`jq .latestExecution.status ${{github.workspace}}/state.json`
+STATE=`jq .latestExecution.status state.json`
 if [ "$STATE" == "InProgress" ]; then
   echo "InProgress......"
   timeout 50 
   aws codepipeline get-pipeline-state --name buildpipe > state.json
   sudo chmod 777 state.json
-  STATE=`jq .latestExecution.status ${{github.workspace}}/state.json`  
+  STATE=`jq .latestExecution.status state.json`  
 fi
-if [ "$STATE" == "InProgress" ]; then
-  echo "InProgress......"
-  timeout 50 
-  aws codepipeline get-pipeline-state --name buildpipe > state.json
-  sudo chmod 777 state.json
-  STATE=`jq .latestExecution.status ${{github.workspace}}/state.json`  
-fi
-if [ "$STATE" == "Succeeded" ]; then
-  echo "Pipeline is Succeeded" 
-if [ "$STATE" == "Failed" ]; then
-  echo "Pipeline execution failed !!"
-fi
-
