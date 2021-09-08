fba9594c4c0da9a3798b5f3b3f3d096c9ada9215
Mahima-Sahu <52346587+Mahima-Sahu@users.noreply.github.com>
2021-09-08 22:18:19 +0530

    Update unix_pipe.sh

--- unix_pipe.sh
+++ unix_pipe.sh
@@ -2,17 +2,17 @@
 aws codepipeline start-pipeline-execution --name buildpipe
 touch state.json
 aws codepipeline get-pipeline-state --name buildpipe > state.json
-SOURCE=`jq .stageStates[0].latestExecution.status state.json`
-DEPLOY=`jq .stageStates[2].latestExecution.status state.json`
-BUILD=`jq .stageStates[1].latestExecution.status state.json`
+SOURCE=`jq -r .stageStates[0].latestExecution.status state.json`
+DEPLOY=`jq -r .stageStates[2].latestExecution.status state.json`
+BUILD=`jq -r .stageStates[1].latestExecution.status state.json`
 while [ $SOURCE = "InProgress" ] || [ $DEPLOY = "InProgress" ] || [ $BUILD = "InProgress" ]
 do
     echo "Pipeline is InProgress......"
     sleep 50s
     aws codepipeline get-pipeline-state --name buildpipe > state.json
-    SOURCE=`jq .stageStates[0].latestExecution.status state.json`
-    DEPLOY=`jq .stageStates[2].latestExecution.status state.json`
-    BUILD=`jq .stageStates[1].latestExecution.status state.json`
+    SOURCE=`jq -r .stageStates[0].latestExecution.status state.json`
+    DEPLOY=`jq -r .stageStates[2].latestExecution.status state.json`
+    BUILD=`jq -r .stageStates[1].latestExecution.status state.json`
 done
 if [ $SOURCE = "Succeeded" ] && [ $DEPLOY = "Succeeded" ] && [ $BUILD = "Succeeded" ]
 then
