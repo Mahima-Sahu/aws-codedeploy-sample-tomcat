af16bf2a1fef34b8d1d81526ddd29f3961014269
Mahima-Sahu <52346587+Mahima-Sahu@users.noreply.github.com>
2021-09-08 22:03:40 +0530

    Update unix_pipe.sh

--- unix_pipe.sh
+++ unix_pipe.sh
@@ -5,18 +5,19 @@ aws codepipeline get-pipeline-state --name buildpipe > state.json
 SOURCE=`jq .stageStates[0].latestExecution.status state.json`
 DEPLOY=`jq .stageStates[2].latestExecution.status state.json`
 BUILD=`jq .stageStates[1].latestExecution.status state.json`
-while ([ $SOURCE = "InProgress" ] || [ $DEPLOY = "InProgress" ] || [ $BUILD = "InProgress" ])
+while [ $SOURCE = "InProgress" ] || [ $DEPLOY = "InProgress" ] || [ $BUILD = "InProgress" ]
 do
     echo "Pipeline is InProgress......"
     sleep 50s
     aws codepipeline get-pipeline-state --name buildpipe > state.json
     SOURCE=`jq .stageStates[0].latestExecution.status state.json`
     DEPLOY=`jq .stageStates[2].latestExecution.status state.json`
+    BUILD=`jq .stageStates[1].latestExecution.status state.json`
 done
-if ([ $SOURCE = "Succeeded" ] && [ $DEPLOY = "Succeeded" ] && [ $BUILD = "Succeeded" ])
+if [ $SOURCE = "Succeeded" ] && [ $DEPLOY = "Succeeded" ] && [ $BUILD = "Succeeded" ]
 then
     echo "Pipeline-- Succeeded --"
-elif ([ $SOURCE = "Stopped" ] || [ $DEPLOY = "Stopped" ] || [ $BUILD = "Stopped" ])
+elif [ $SOURCE = "Stopped" ] || [ $DEPLOY = "Stopped" ] || [ $BUILD = "Stopped" ]
 then
     echo "Pipeline is-- Stopped --"
     exit 1
