10968c9a6f90ba0637ba4254a5ceffd211fd8505
Mahima-Sahu <52346587+Mahima-Sahu@users.noreply.github.com>
2021-09-09 10:06:59 +0530

    Update unix_pipe.sh

--- unix_pipe.sh
+++ unix_pipe.sh
@@ -2,17 +2,22 @@
 aws codepipeline start-pipeline-execution --name buildpipe
 touch state.json
 aws codepipeline get-pipeline-state --name buildpipe > state.json
+declare -i count=3
 SOURCE=`jq -r .stageStates[0].latestExecution.status state.json`
 DEPLOY=`jq -r .stageStates[2].latestExecution.status state.json`
 BUILD=`jq -r .stageStates[1].latestExecution.status state.json`
 while [ $SOURCE = "InProgress" ] || [ $DEPLOY = "InProgress" ] || [ $BUILD = "InProgress" ]
 do
     echo "Pipeline is InProgress......"
-    sleep 50s
+    sleep 65s
     aws codepipeline get-pipeline-state --name buildpipe > state.json
-    SOURCE=`jq -r .stageStates[0].latestExecution.status state.json`
-    DEPLOY=`jq -r .stageStates[2].latestExecution.status state.json`
-    BUILD=`jq -r .stageStates[1].latestExecution.status state.json`
+    SOURCE=`jq .stageStates[0].latestExecution.status state.json`
+    DEPLOY=`jq .stageStates[2].latestExecution.status state.json`
+    BUILD=`jq .stageStates[1].latestExecution.status state.json`
+    if [ $count = 3 ]
+    then 
+      break
+    fi
 done
 if [ $SOURCE = "Succeeded" ] && [ $DEPLOY = "Succeeded" ] && [ $BUILD = "Succeeded" ]
 then
@@ -20,6 +25,10 @@ then
 elif [ $SOURCE = "Stopped" ] || [ $DEPLOY = "Stopped" ] || [ $BUILD = "Stopped" ]
 then
     echo "Pipeline is-- Stopped --"
+    exit 1
+elif [ $SOURCE = "InProgress" ] || [ $DEPLOY = "InProgress" ] || [ $BUILD = "InProgress" ]
+then
+    echo "Pipeline is-- InProgress for very long time > $count minutes --"
     exit 1
 else
     echo "Pipeline-- Failed --!!"
